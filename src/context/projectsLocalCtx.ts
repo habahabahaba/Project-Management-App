6bb58be00de4f48d7b683cde6be4db6f55c1a256
Alex S <habahabahaba@outlook.com>
2025-03-21 21:21:29 +0400

    Add initState to the projectsLocalCtx in order to avoid null-checks later.

--- src/context/projectsLocalCtx.ts
+++ src/context/projectsLocalCtx.ts
@@ -1,16 +1,22 @@
 // React:
-import { createContext } from 'react';
+import { createContext } from "react";
 // Types, interfaces and enumns:
-import type { Dispatch, SetStateAction } from 'react';
-import type { Projects } from './projects.types';
-import type { ProjectsAction } from './projectsLocalReducer';
+import type { Dispatch, SetStateAction } from "react";
+import type { Projects } from "./projects.types";
+import type { ProjectsAction } from "./projectsLocalReducer";
 type projectsLocalCtxValue = {
   localState: Projects;
   localDispatch: Dispatch<ProjectsAction>;
   selectedId: string | null;
   selectId: Dispatch<SetStateAction<string | null>>;
-} | null;
+};
 
-const projectsLocalCtx = createContext<projectsLocalCtxValue>(null);
+const initState: projectsLocalCtxValue = {
+  localState: [],
+  localDispatch: () => {},
+  selectedId: null,
+  selectId: () => {},
+};
+const projectsLocalCtx = createContext<projectsLocalCtxValue>(initState);
 
 export default projectsLocalCtx;
