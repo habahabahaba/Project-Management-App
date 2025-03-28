0d5fe3db3c49bb75c6e612e5edde1255672249e4
Alex S <habahabahaba@outlook.com>
2025-03-28 05:28:00 +0400

    Make rand part of generateId output longer.

--- src/utils/generateId.ts
+++ src/utils/generateId.ts
@@ -1,6 +1,6 @@
 export default function generateId(): string {
   const now = Date.now().toString().slice(5);
-  const rand = Math.random().toFixed(5).toString().slice(2);
+  const rand = Math.random().toFixed(8).toString().slice(2);
 
   console.log("[generateId]", "now:", now, "rand:", rand);
 
